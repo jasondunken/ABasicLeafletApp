671b83d0a98b3a8e00d343145c76c71ba83be6c6
jasondunken <jasondunken@yahoo.com>
2023-06-16 22:44:55 -0400

    refactor #6 => lots of little things, almost done

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,9 @@ const url_NP21_catchments =
     "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/Catchments_NP21_Simplified/MapServer/0";
 // WBD - HUC_8
 const url_NP21_huc8 = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/2";
+request1.innerHTML = url_NP21_flowlines;
+request2.innerHTML = url_NP21_catchments;
+request3.innerHTML = url_NP21_huc8;
 
 // point indexing service
 const point_indexing_service_url = "https://ofmpub.epa.gov/waters10/PointIndexing.Service";
@@ -38,7 +41,7 @@ const up_down_service_url = "https://ofmpub.epa.gov/waters10/UpstreamDownstream.
 const url_NP21_monitor_locations =
     "https://watersgeo.epa.gov/arcgis.rest/services/NHDPlus_NP21/STORET_NP21/MapServer/0";
 // STORET webservices
-const url_stations_base = "https://www.waterqualitydata.us/data/Station/search?";
+const url_stations_base = "https://www.waterqualitydata.us/data/Station/search";
 
 // build base map
 const map = L.map("map").setView([dLat, dLng], dZoom);
@@ -65,12 +68,13 @@ catchments_ml.setStyle({
     fillOpacity: 0,
 });
 
-const boundaries_ml = L.esri
+const boundaries_huc8_ml = L.esri
     .featureLayer({
         url: url_NP21_huc8,
+        minZoom: 7,
     })
     .addTo(map);
-boundaries_ml.setStyle({
+boundaries_huc8_ml.setStyle({
     color: "green",
     fillOpacity: 0,
 });
@@ -89,51 +93,54 @@ const beerCanIcon = L.icon({
     popupAnchor: [0, 32],
 });
 
-// map interaction
-function onMapClick(clickEvent) {
-    station_sites.remove();
-    origin_marker.remove();
-    const latlng = clickEvent.latlng;
-    origin_marker = L.marker([latlng.lat, latlng.lng]).bindPopup(
-        `Search origin <br> lat ${latlng.lat.toFixed(6)} <br> lng ${latlng.lng.toFixed(6)}`
-    );
-    origin_marker.addTo(map);
+const snaplineColor = "#ff0000";
+const streamStartColor = "#00F000";
+const streamSegmentColor = "#00F0F0";
 
-    // update the form
-    input_lat.value = latlng.lat;
-    input_lng.value = latlng.lng;
-}
-map.on("click", onMapClick);
+map.on("click", (event) => {
+    setOrigin(event.latlng);
+});
+
+map.on("overlayadd", (event) => {
+    streamline_ml.bringToFront();
+});
+map.on("layeradd", (event) => {
+    streamline_ml.bringToFront();
+});
 
 // force update the map in case something in the dom has changed
 // sometimes with out this you can end up getting blank map tiles until you move/scale the map
 map.invalidateSize();
 
-const snapline = L.geoJson().addTo(map);
-const streamline = L.geoJson().addTo(map);
-const searchStartStream = L.geoJson().addTo(map);
-const station_layer = L.geoJson().addTo(map);
+const origin_marker = L.marker();
+const snapline_ml = L.layerGroup();
+const streamline_ml = L.featureGroup(); // needs to be a featureGroup is you want to use getBounds on it
+const stream_events_ml = L.layerGroup();
+const monitoring_stations_ml = L.featureGroup();
 
-const userLayers = {
-    snapline: L.geoJson().addTo(map),
-    streamline: L.geoJson().addTo(map),
-    searchStartStream: L.geoJson().addTo(map),
-    station_layer: L.geoJson().addTo(map),
+const overlayMaps = {
+    "Flow Lines": flowlines_ml,
+    "Catchment Boundaries": catchments_ml,
+    "HUC8 Boundaries": boundaries_huc8_ml,
+    "Stream Events": stream_events_ml,
+    "Monitoring Stations": monitoring_stations_ml,
 };
+const layer_options = L.control.layers(null, overlayMaps);
+layer_options.addTo(map);
 
-let layer_options;
-function updateLayerGroups() {
-    if (layer_options) {
-        layer_options.remove();
-    }
-    let overlayMaps = {
-        "Flow Lines": flowlines_ml,
-        Catchments: catchments_ml,
-        "HUC8 Boundaries": boundaries_ml,
-        "Station Sites": station_sites,
-    };
-    layer_options = L.control.layers(null, overlayMaps);
-    layer_options.addTo(map);
+function setOrigin(latLng) {
+    origin_marker.unbindPopup();
+    snapline_ml.clearLayers();
+    streamline_ml.clearLayers();
+    stream_events_ml.clearLayers();
+
+    origin_marker.setLatLng([latLng.lat, latLng.lng]);
+    origin_marker.bindPopup(`Search origin <br> lat ${latLng.lat.toFixed(6)} <br> lng ${latLng.lng.toFixed(6)}`);
+    origin_marker.addTo(map);
+
+    // update the form
+    input_lat.value = latLng.lat;
+    input_lng.value = latLng.lng;
 }
 
 async function getStreamNetwork() {
@@ -152,6 +159,8 @@ function callPointIndexingService() {
         pOutputPathFlag: "TRUE",
         pReturnFlowlineGeomFlag: "FALSE",
     };
+    request4.innerHTML = buildRequest(point_indexing_service_url, data);
+
     return new Promise((resolve, reject) => {
         L.esri.get(point_indexing_service_url, data, (error, response) => {
             if (error) {
@@ -191,6 +200,8 @@ function callUpDownService(pointIndex) {
         optOutPruneNumber: 8,
         optOutCS: "SRSNAME=urn:ogc:def:crs:OGC::CRS84",
     };
+    request5.innerHTML = buildRequest(up_down_service_url, data);
+
     return new Promise((resolve, reject) => {
         L.esri.get(up_down_service_url, data, (error, response) => {
             if (error) {
@@ -205,146 +216,129 @@ function callUpDownService(pointIndex) {
                 }
                 return;
             }
-            addStreamAndEvents(comid, response.output.flowlines_traversed, response.output.events_encountered);
+            addStreamLine(comid, response.output.flowlines_traversed);
+            addStreamEvents(response.output.events_encountered);
+            output1.innerHTML = `comids found ${response.output.flowlines_traversed?.length}`;
             resolve(response.output);
         });
     });
 }
 
 function addSnapLine(pointIndex) {
-    snapline.clearLayers();
-
-    const feature = geojson2feature(pointIndex.indexing_path);
-    snapline.addData(feature).setStyle({
-        color: "#FF0000",
-        fillColor: "#FF0000",
+    const feature = polyline(pointIndex.indexing_path.coordinates);
+    feature.setStyle({
+        color: snaplineColor,
+        weight: 4,
     });
+    snapline_ml.addLayer(feature);
+    snapline_ml.addTo(map);
 }
 
-function addStreamAndEvents(pPointComid, flowlines, events) {
-    console.log("pPointComid: ", pPointComid);
-    streamline.clearLayers();
-    searchStartStream.clearLayers();
-    station_layer.clearLayers();
-
-    const streamColor = "#00F0F0";
-    const startColor = "#00F000";
-
+function addStreamLine(pPointComid, flowlines) {
     for (let i in flowlines) {
         const flowline = flowlines[i];
-        let tmp_feature = geojson2feature(flowline.shape, "NHDFlowline " + flowline.comid, i + 10000);
-        let color = streamColor;
-        console.log("fLine comid: ", flowline.comid);
-        if (pPointComid === flowline.comid) {
-            color = startColor;
+        const feature = polyline(flowline.shape.coordinates);
+        let color = streamSegmentColor;
+        if (flowline.comid === pPointComid) {
+            color = streamStartColor;
         }
-        streamline.addData(tmp_feature).setStyle({
+        feature.setStyle({
             color,
             weight: 4,
         });
+        streamline_ml.addLayer(feature);
+        feature.bindTooltip(`comid ${flowline.comid}`);
     }
+    streamline_ml.addTo(map);
+    map.fitBounds(streamline_ml.getBounds());
+}
 
+function addStreamEvents(events) {
     // events are "gages" encountered (pEventList items)
     for (let i = 0; i < events?.length; i++) {
         const event = events[i];
-        const latlng = {
-            lat: event.shape.coordinates[1],
-            lng: event.shape.coordinates[0],
-        };
-        const marker = L.marker(latlng, { icon: dropperIcon });
+        const latLng = flipCoord(event.shape.coordinates);
+        const marker = L.marker(latLng, { icon: dropperIcon });
         marker.markerInfo = `
             comid ${event.comid} <br>
             gageID ${event.source_featureid} <br>
-            origin ${event.source_originator}`;
+            origin ${event.source_originator}
+        `;
         marker.bindPopup(marker.markerInfo);
         marker.on("click", (clickEvent) => {
-            setOutput1(clickEvent.target.markerInfo);
+            output1.innerHTML = clickEvent.target.markerInfo;
         });
-        marker.addTo(station_layer);
+        marker.addTo(stream_events_ml);
     }
-    map.fitBounds(streamline.getBounds(), {
-        maxZoom: 13,
-    });
+    stream_events_ml.addTo(map);
 }
 
-// Function to wrap GeoJSON geometry into a vanilla feature
-function geojson2feature(p_geojson, p_popup_value, p_id) {
-    if (p_geojson === undefined) {
-        return null;
-    }
+async function getMonitoringStations() {
+    monitoring_stations_ml.clearLayers();
 
-    if (p_id === undefined || p_id === null) {
-        p_id = 0;
-    }
-
-    const p_feature = {
-        type: "Feature",
-        properties: {
-            id: p_id,
-            popupValue: p_popup_value,
-        },
-        geometry: p_geojson,
+    const data = {
+        lat: input_lat.value,
+        long: input_lng.value,
+        within: input_within.value,
+        mimeType: input_mimeType.value,
     };
 
-    return p_feature;
+    let url_station_request = buildRequest(url_stations_base, data);
+    request6.innerHTML = url_station_request;
+
+    const response = await fetch(url_station_request);
+    const stationData = await response.json();
+    output2.innerHTML = `stations found ${stationData?.features?.length}`;
+
+    for (let feature of stationData?.features) {
+        const marker = L.marker(flipCoord(feature.geometry.coordinates), { icon: beerCanIcon });
+        marker.markerInfo = `
+            huc8 ${feature.properties.HUCEightDigitCode} <br>
+            locationID ${feature.properties.MonitoringLocationIdentifier} <br>
+            location ${feature.properties.MonitoringLocationName} <br>
+            org ${feature.properties.OrganizationFormalName}
+        `;
+        marker.bindPopup(marker.markerInfo);
+        marker.addTo(monitoring_stations_ml);
+        marker.on("click", (e) => {
+            output2.innerHTML = e.target.markerInfo;
+        });
+    }
+    monitoring_stations_ml.addTo(map);
 }
 
-const http = new XMLHttpRequest();
-let stations = "";
-let stations_geojson = "";
-let station_sites = L.geoJSON();
-let origin_marker = L.marker();
-
-// // connected to form send button
-function getMonitoringStations() {
-    let url_station_request = buildRequest();
-
-    http.open("GET", url_station_request, false);
-    http.send();
-    stations = http.responseText;
-    stations_geojson = JSON.parse(stations);
-    station_sites = L.geoJSON(stations_geojson, {
-        pointToLayer: function (feature, latlng) {
-            let marker = L.marker(latlng, { icon: beerCanIcon });
-            marker.markerInfo = `huc8 ${feature.properties.HUCEightDigitCode} <br> gageID ${feature.properties.MonitoringLocationIdentifier}`;
-            marker.bindPopup(marker.markerInfo);
-            marker.on("click", (e) => {
-                setOutput2(e.target.markerInfo);
-            });
-            return marker;
-        },
-    });
-    station_sites.addTo(map);
-    updateDataFields();
-    updateLayerGroups();
+function buildRequest(url, data) {
+    let requestURL = url + "?";
+    const props = Object.keys(data);
+    requestURL += `${props[0]}=${data[props[0]]}`;
+    for (let i = 1; i < props.length; i++) {
+        requestURL += `&${props[i]}=${data[props[i]]}`;
+    }
+    return requestURL;
 }
 
-function buildRequest() {
-    // validate request
-    const lat = input_lat.value;
-    const lng = input_lng.value;
-    const within = input_within.value;
-    const mimeType = input_mimeType.value;
+/**
+ * the output from the endpoints used to gather data here
+ * return coordinates in [longitude, latitude] format,
+ * Leaflet expects them in [latitude, longitude] format
+ */
 
-    return url_stations_base + "lat=" + lat + "&long=" + lng + "&within=" + within + "&mimeType=" + mimeType;
+// polyline is just a wrapper around Leaflet.polyline that
+// lets you build a polyline layer with coords in inverted order
+function polyline(coords, options) {
+    return L.polyline(lngLatArr2LatLngArr(coords, options));
 }
 
-function updateDataFields() {
-    request1.innerHTML = url_NP21_flowlines;
-    request2.innerHTML = url_NP21_catchments;
-    request3.innerHTML = url_NP21_huc8;
-    request4.innerHTML = "url&stream_request";
-    output1.innerHTML = http.status;
-    request5.innerHTML = "url&station_request";
-    output2.innerHTML = http.status;
+function lngLatArr2LatLngArr(coords) {
+    for (let coord of coords) {
+        coord = flipCoord(coord);
+    }
+    return coords;
 }
 
-function setOutput1(info) {
-    output1.innerHTML = info;
+function flipCoord(coord) {
+    const temp = coord[0];
+    coord[0] = coord[1];
+    coord[1] = temp;
+    return coord;
 }
-
-function setOutput2(info) {
-    output2.innerHTML = info;
-}
-updateDataFields();
-updateLayerGroups();
